17cb37e1831399d8cfd514246ba8471d0668ae33
SandeshVakale <sandyrv.vakale@gmail.com>
2020-09-01 11:25:54 +0200

    styling changes

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,7 +5,7 @@ import '../App.css';
 function Grid() {
     return (
             <div className='Grid'>
-                <div className='Block'>This is a page for beauty Product search</div>
+                <div className='Block'><div className='text'>This is a page for beauty Product search</div></div>
                 <img className='Block' src={logo} />
             </div>
     );
