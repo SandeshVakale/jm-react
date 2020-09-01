a69b8b18cee3c7dc69e74827cf867d7c38a5d0d9
SandeshVakale <sandyrv.vakale@gmail.com>
2020-09-01 21:40:12 +0200

    Updates

---
+++ precache-manifest.a6c9fd883ec537c43b385e9106e039d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb35df3faf6e66d928b17240afc623b",
+    "url": "/jm-react/index.html"
+  },
+  {
+    "revision": "357da9786ea355677ec1",
+    "url": "/jm-react/static/css/main.1f28898b.chunk.css"
+  },
+  {
+    "revision": "21e0a33e8031e491546f",
+    "url": "/jm-react/static/js/2.203dff59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jm-react/static/js/2.203dff59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357da9786ea355677ec1",
+    "url": "/jm-react/static/js/main.93c8626d.chunk.js"
+  },
+  {
+    "revision": "3ba6ac0f8011ade969e0",
+    "url": "/jm-react/static/js/runtime-main.0222d074.js"
+  },
+  {
+    "revision": "9840c5061ed354262e36b64795a50d54",
+    "url": "/jm-react/static/media/beauty-products.9840c506.jpg"
+  }
+]);
